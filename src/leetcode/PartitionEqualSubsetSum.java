9f0c0c8ec81b7e9afa9f95a756965849f8c9873e
gunelmirzoeva <mirzoevagunel20@gmail.com>
2025-04-07 19:42:09 +0400

    Add leetcode question 416

---
+++ src/leetcode/PartitionEqualSubsetSum.java
@@ -0,0 +1,33 @@
+package leetcode;
+
+import java.util.Scanner;
+
+// https://leetcode.com/problems/partition-equal-subset-sum/description/?envType=daily-question&envId=2025-04-07
+public class PartitionEqualSubsetSum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println(canPartition(arr));
+
+    }
+    public static boolean canPartition(int[] nums) {
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+        if (sum % 2 != 0) return false;
+        int target = sum / 2;
+        boolean[] dp = new boolean[target + 1];
+        dp[0] = true;
+        for (int num : nums) {
+            for (int i = target; i >= num; i--) {
+                dp[i] = dp[i] || dp[i - num];
+            }
+        }
+        return dp[target];
+    }
+}
